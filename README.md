c05bf31b3ebd0ace77215bb98c52475388deb190
igraczech <suculent@me.com>
2019-07-02 16:00:32 +0200

    Codacy style fixes

---
+++ README.md
@@ -0,0 +1,21 @@
+# DVNA
+
+A Damn Vulnerable Node.js Application.
+
+This is an example project for Automated Security Testing
+
+# Sonarcloud
+
+[![Quality gate](https://sonarcloud.io/api/project_badges/quality_gate?project=corpus-solutions-dvna)](https://sonarcloud.io/dashboard?id=corpus-solutions-dvna)
+
+[![Code Smells](https://sonarcloud.io/api/project_badges/measure?project=corpus-solutions-dvna&metric=code_smells)](https://sonarcloud.io/dashboard?id=corpus-solutions-dvna)
+
+https://sonarcloud.io/dashboard?id=corpus-solutions-dvna
+
+# LGTM
+
+https://lgtm.com/projects/g/corpus-solutions/dvna/alerts/?mode=list
+
+# Codescene
+
+[![](https://codescene.io/projects/5182/status.svg) Get more details at **codescene.io**.](https://codescene.io/projects/5182/jobs/latest-successful/results)
--- app.js
+++ app.js
@@ -1,31 +1,62 @@
-const express = require('express');
-const fsx = require('fs-extra');
+const express = require("express");
+const fsx = require("fs-extra");
 const exec = require("child_process");
 
 const app = express();
 const port = process.env.port || 8080;
 
-function get_file_contents(path) {
-  const object = fsx.readFileSync(path)
+/* CWE-770, CWE-307, CWE-400: set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+// apply rate limiter to all requests
+app.use(limiter);
+*/
+
+function getFileContents(path) {
+
+  /* CWE-22, CWE-23, CWE-36, CWE-73, CWE-99: Validate user input before using
+   it to construct a file path, either using an off-the-shelf library like
+   the sanitize-filename npm package, or by performing custom validation.
+
+   Ideally, follow these rules:
+   * Do not allow more than a single "." character.
+   * Do not allow directory separators such as "/" or "\" (depending on the file system).
+   * Do not rely on simply replacing problematic sequences such as "../".
+     For example, after applying this filter to ".../...//", the resulting
+     string would still be "../".
+   * Use a whitelist of known good patterns.
+  */
+
+  const object = fsx.readFileSync(path);
   return object;
 }
 
 function getFile(req, res) {
   const input = req.params.arg;
   console.log("Unvalidated user input: " + input);
-  const language_file = get_file_contents("./languages/" + input);
+  const languageFile = getFileContents("./languages/" + input);
 
   // Chybějící sanitace:
-  // language_file = language_file.replace(/^\//, "");
-  console.log(language_file.toString());
-  res.end(language_file);
+  // languageFile = languageFile.replace(/^\//, "");
+  console.log(languageFile.toString());
+  res.end(languageFile);
   // TODO: Take (non-)validated user input and use execSync to create shell injection vulnerability
-};
+}
 
 function runJob(req, res) {
   const input = req.params.job;
   const command = "git clone " + input;
   console.log("Command with unvalidated user input: "+command);
+
+  /* CWE-78, CWE-88: If possible, use hard-coded string literals to specify the command
+     to run or library to load. Instead of passing the user input directly
+    to the process or library function, examine the user input and then choose
+    among hard-coded string literals.
+    If the applicable libraries or commands cannot be determined at compile time,
+    then add code to verify that the user input string is safe before using it. */
   var result = exec.execSync(command).toString();
   console.log(result);
   res.end(result);
@@ -43,7 +74,7 @@ function runJob(req, res) {
 // curl http://localhost:8080/english%2F..%2F..
 // curl http://localhost:8080/english%2F..%2Fczech
 
-app.get('/:arg', getFile);
+app.get("/:arg", getFile);
 
 //
 // Example 2: shell injection (root takeover)
@@ -54,7 +85,7 @@ app.get('/:arg', getFile);
 
 // Use-cases
 
-app.get('/job/:job', runJob);
+app.get("/job/:job", runJob);
 
 // Server listener
 
